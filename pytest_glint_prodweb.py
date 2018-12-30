80e4af1542d8550769867754c1f04558a83bab74
Eric Medina <eric.medina.atienza@gmail.com>
2018-12-30 20:30:06 +0800

    Initial commit

---
+++ pytest_glint_prodweb.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from unittest.mock import patch
+
+import pytest
+
+
+@pytest.fixture
+def sa_session():
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import scoped_session, sessionmaker
+
+    e = create_engine('sqlite:///:memory:')
+    e.execute('PRAGMA foreign_keys=ON')
+    session = scoped_session(sessionmaker(autoflush=True, autocommit=False, bind=e))
+
+    try:
+        import prodweb.database as db
+        db.Session = session
+    except ImportError:
+        pass
+    yield session
+
+    session.remove()
+    e.dispose()
+
+
+@pytest.fixture
+def cp_session():
+    from cherrypy.lib.sessions import RamSession
+    sess_mock = RamSession()
+    patcher = patch('cherrypy.session', sess_mock, create=True)
+    patcher.start()
+    yield sess_mock
+    patcher.stop()
+
+
+@pytest.fixture
+def cp_request(sa_session):
+    patcher = patch('cherrypy.request', autospec=True)
+    r = patcher.start()
+    r.db = sa_session
+    yield r
+    patcher.stop()
